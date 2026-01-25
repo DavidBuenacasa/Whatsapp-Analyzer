66da036d3663140fc6cd499e47ec79ecbaaecb85
David Buenacasa <davidbuenacasalopez12@gmail.com>
2026-01-25 23:18:31 +0100

    Update Inicio.tsx

--- src/Inicio.tsx
+++ src/Inicio.tsx
@@ -110,7 +110,7 @@ function Inicio() {
       <div className="flex h-screen w-full flex-col items-center justify-center gap-2">
         {/*Titulo e Imagen principal*/}
         <div className="titulo">
-          <img src="/img/whatsapp.svg" id="logo-whatsapp"></img>
+          
           <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
             {es_text["main-title"]}{" "}
             <span className="text-blue-600 dark:text-blue-500">
